efecc2c41c141a3a6ea5a5d6078af5aebcd7f500
unknown <agustinadibella@gmail.com>
2024-07-01 23:03:30 -0300

    Entrega.1.1

--- js/wg.js
+++ js/wg.js
@@ -6,62 +6,62 @@ function precioEnCuotas(valorTotal, cantCuotas) {
     return valorTotal / cantCuotas;
 }
 
+function decimal(numero){
+    return numero.toLocaleString('es-AR',{minimunFractionDigits:2, maximumFractionDigits:2});
+}
+
 function valor() {
     let opcionExperiencia = prompt('Bienvenido a WeGift, ¿qué experiencia desea disfrutar? \n 1) Cena en Rooftop $40000 por persona \n 2) Merienda en el Hotel Duhau $15000 por persona \n 3) Almuerzo con vista al Río $30000 por persona\n\nIngrese la opción deseada');
-    if (opcionExperiencia === '1') {
-        let precioPorPersona = 40000;
-        let cantPersonas = parseInt(prompt('Por favor ingrese la cantidad de personas que desea'))
-        let valorTotal = totalExperiencia(precioPorPersona, cantPersonas);
-        let cantCuotas = parseInt(prompt('La compra se puede realizar en 1 , 3 y 6 Cuotas sin interes \n Ingrese en cuantas cuotas desea realizar la compra'));
-        if (cantCuotas === 1) {
-            let valorEnCuotas = precioEnCuotas(valorTotal, cantCuotas);
-            alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en una cuota de $${valorEnCuotas}`);
+        if (opcionExperiencia === '1') {
+            let precioPorPersona = 40000;
+            let cantPersonas = parseInt(prompt('Por favor ingrese la cantidad de personas que desea'))
+            let valorTotal = totalExperiencia(precioPorPersona, cantPersonas);
+            let cantCuotas = parseInt(prompt('La compra se puede realizar en 1 , 3 y 6 Cuotas sin interes \n Ingrese en cuantas cuotas desea realizar la compra'));
+            if (cantCuotas === 1) {
+                let valorEnCuotas = precioEnCuotas(valorTotal, cantCuotas);
+                alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en una cuota de $${decimal(valorEnCuotas)}`);
+            } else if (cantCuotas === 3 || cantCuotas === 6) {
+                let valorEnCuotas = precioEnCuotas(valorTotal, cantCuotas);
+                alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en ${cantCuotas} cuotas de $${decimal(valorEnCuotas)}`);
+            }
+            else {
+                alert('La cantidad de cuotas es inválida')
+            }
+        } else if (opcionExperiencia === '2') {
+            let precioPorPersona = 15000;
+            let cantPersonas = parseInt(prompt('Por favor ingrese la cantidad de personas que desea'))
+            let valorTotal = totalExperiencia(precioPorPersona, cantPersonas);
+            let cantCuotas = parseInt(prompt('La compra se puede realizar en 1 , 3 y 6 Cuotas sin interes \n Ingrese en cuantas cuotas desea realizar la compra'));
+            if (cantCuotas === 1) {
+                let valorEnCuotas = precioEnCuotas(valorTotal, cantCuotas);
+                alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en una cuota de $${decimal(valorEnCuotas)}`);
         } else if (cantCuotas === 3 || cantCuotas === 6) {
             let valorEnCuotas = precioEnCuotas(valorTotal, cantCuotas);
-            alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en ${cantCuotas} cuotas de $${valorEnCuotas}`);
+            alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en ${cantCuotas} cuotas de $${decimal(valorEnCuotas)}`);
         }
         else {
             alert('La cantidad de cuotas es inválida')
         }
-
-
-    } else if (opcionExperiencia === '2') {
-        let precioPorPersona = 15000;
-        let cantPersonas = parseInt(prompt('Por favor ingrese la cantidad de personas que desea'))
-        let valorTotal = totalExperiencia(precioPorPersona, cantPersonas);
-        let cantCuotas = parseInt(prompt('La compra se puede realizar en 1 , 3 y 6 Cuotas sin interes \n Ingrese en cuantas cuotas desea realizar la compra'));
-        if (cantCuotas === 1) {
-            let valorEnCuotas = precioEnCuotas(valorTotal, cantCuotas);
-            alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en una cuota de $${valorEnCuotas}`);
-        } else if (cantCuotas === 3 || cantCuotas === 6) {
-            let valorEnCuotas = precioEnCuotas(valorTotal, cantCuotas);
-            alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en ${cantCuotas} cuotas de $${valorEnCuotas}`);
-        }
-        else {
-            alert('La cantidad de cuotas es inválida')
-        }
-
-
     } else if (opcionExperiencia === '3') {
         let precioPorPersona = 30000;
         let cantPersonas = parseInt(prompt('Por favor ingrese la cantidad de personas que desea'))
         let valorTotal = totalExperiencia(precioPorPersona, cantPersonas);
         let cantCuotas = parseInt(prompt('La compra se puede realizar en 1 , 3 y 6 Cuotas sin interes \n Ingrese en cuantas cuotas desea realizar la compra'));
-        if (cantCuotas === 1) {
+            if (cantCuotas === 1) {
             let valorEnCuotas = precioEnCuotas(valorTotal, cantCuotas);
-            alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en una cuota de $${valorEnCuotas}`);
-        } else if (cantCuotas === 3 || cantCuotas === 6) {
+            alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en una cuota de $${decimal(valorEnCuotas)}`);
+            } else if (cantCuotas === 3 || cantCuotas === 6) {
             let valorEnCuotas = precioEnCuotas(valorTotal, cantCuotas);
-            alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en ${cantCuotas} cuotas de $${valorEnCuotas}`);
-        }
-        else {
+            alert(`el Valor total de la Experiencia es de $${valorTotal}, lo abonaras en ${cantCuotas} cuotas de $${decimal(valorEnCuotas)}`);
+            }
+            else {
             alert('La cantidad de cuotas es inválida')
-        }
+            }
     }
-
-    else {
+        else {
         (opcionExperiencia === '' || NaN || opcionExperiencia < 4 || opcionExperiencia === 'undifined')
-        alert('Elija una opción valida');
+        alert('Elija una opción válida')
+        valor();
       }
 }
 
